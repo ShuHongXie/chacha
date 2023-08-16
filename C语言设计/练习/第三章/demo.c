1043971be1fbef9572f333fef9ddf8562ea8bc48
shuhongxie <384180258@qq.com>
2023-08-16 23:39:53 +0800

    feat: 增加

--- C语言设计/练习/第三章/demo.c
+++ C语言设计/练习/第三章/demo.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
+#include <math.h>
 
 void main()
 {
-    // doubleèµ‹å€¼float
+    // double¸³Öµfloat
     // float f;
     // double d = 123.456789e100;
     // f = d;
     // printf("f=%f", f);
 
-    // å­—ç¬¦ï¼Œå­—ç¬¦ä¸²çš„è¾“å…¥è¾“å‡º
-    char a, b, c;
-    a = 'B', b = 'O', c = 'Y';
-    putchar(a);
-    putchar(b);
-    putchar(c);
+    // ×Ö·û£¬×Ö·û´®µÄÊäÈëÊä³ö
+    // char a, b, c;
+    // a = getchar();
+    // b = getchar();
+    // c = getchar();
+    // putchar(a);
+    // putchar(b);
+    // putchar(c);
+    // putchar(getchar());
+    // putchar(getchar());
+    // putchar(getchar());
+
+    // int a, b, c;
+    // scanf("%d%d%d", &a, &b, &c);
+    // printf("a=%d,b=%d,c=%d\n", a, b, c);
+
+    // float f1 = 123.456213213123;
+    // double d1 = 123.232131213213123;
+    // printf("%f\n", d1);
+    // printf("%lf", d1);
+
+    // Èý½ÇÐÎ¼ÆËã
+    // double a, b, c, s, area;
+    // scanf("%lf,%lf,%lf", &a, &b, &c);
+    // s = (a + b + c) / 2.0;
+    // area = sqrt(s * (s - a) * (s - b) * (s - c));
+    // printf("a=%f\nb=%f\nc=%f\narea=%f\n", a, b, c, area);
+    // printf("a   =%9.2f\nb   =%9.2f\nc   =%9.2f\narea=%9.2f\n", a, b, c, area);
+
+    // Ð¡Ð´±ä´óÐ´
+    printf("%c", getchar() + 32);
+    printf("%c", getchar() + 32);
+    printf("%c\n", getchar() + 32);
 }
