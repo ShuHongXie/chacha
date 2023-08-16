a83ca746bc6b2a8b7f6e1bdf1d1aa528af1032e9
shuhongxie <38418258@qq.com>
2023-08-16 17:43:09 +0800

    feat: 增加

---
+++ C语言设计/练习/第三章/demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    // double赋值float
+    // float f;
+    // double d = 123.456789e100;
+    // f = d;
+    // printf("f=%f", f);
+
+    // 字符，字符串的输入输出
+    char a, b, c;
+    a = 'B', b = 'O', c = 'Y';
+    putchar(a);
+    putchar(b);
+    putchar(c);
+}
