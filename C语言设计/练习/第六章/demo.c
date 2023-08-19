fa0e8a02290d38686632219d15d685c80b5d786a
shuhongxie <38418258@qq.com>
2023-08-19 16:56:59 +0800

    feat: 增加

--- C语言设计/练习/第六章/demo.c
+++ C语言设计/练习/第六章/demo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void main()
 {
@@ -11,7 +12,65 @@ void main()
     // char c[10] = "Ha\0ppy";
     // printf("%s", c);
 
-    char str1[10], str2[10];
-    scanf("%s%s", str1, str2);
-    printf("%d", str1);
+    // 输入字符串
+    // char str1[10], str2[10];
+    // scanf("%s%s", str1, str2);
+    // printf("%d", str1);
+
+    // char c1[] = "China";
+    // char c2[] = "abc";
+    // char c3[] = "Abc";
+    // int a = strcmp(c2, c1);
+    // printf("%d", a);
+
+    // 输入3个字符串，判断其大小
+    // char string[20];
+    // char str[3][20];
+    // int i;
+    // for (i = 0; i < 3; i++)
+    // {
+    //     gets(str[i]);
+    // }
+    // if (strcmp(str[0], str[1]) > 0)
+    // {
+    //     strcpy(string, str[0]);
+    // }
+    // else
+    // {
+    //     strcpy(string, str[1]);
+    // }
+    // if (strcmp(str[2], string) > 0)
+    // {
+    //     strcpy(string, str[2]);
+    // }
+    // printf("The largest string is: %s\n", string);
+
+    // 输入一行字符，统计其中有多少个单词，单词之间用空格分隔开
+    // char string[100];
+    // int i, num = 0, word = 0;
+    // char c;
+    // gets(string);
+    // for (i = 0; (c = string[i]) != '\0'; i++)
+    // {
+    //     printf("%d\n", c == ' ');
+    //     if (c == ' ')
+    //     {
+    //         word = 0;
+    //     }
+    //     else if (word == 0)
+    //     {
+    //         word = 1;
+    //         num++;
+    //     }
+    // }
+    // printf("There are %d words in this line,\n", num);
+
+    // gets函数
+    // char s[100];
+    // int c = gets(s);
+    // printf("%s,%d,%d", s, strlen(s), c);
+
+    // strcat函数
+    char str1[30] = "my love ";
+    printf("%s", strcat(str1, "shuhongxie"));
 }
