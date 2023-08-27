1526d32f127cde7be93c917aeaf27f6f94ad7040
shuhongxie <38418258@qq.com>
2023-08-27 16:52:39 +0800

    feat: 增加

--- C语言设计/练习/第十章/demo.c
+++ C语言设计/练习/第十章/demo.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main()
 {
-  FILE *fp;
-  if ((fp = fopen("test.text", "r")) == NULL)
-  {
-    printf("cannot open this file\n");
-    exit(0);
-  }
+  // 简单打开文件操作
+  // FILE *fp;
+  // if ((fp = fopen("test.text", "r")) == NULL)
+  // {
+  //   printf("cannot open this file\n");
+  //   exit(0);
+  // }
+
+  // 从键盘输入字符，然后送到磁盘上去
+  // FILE *fp;
+  // char ch, filename[10];
+  // printf("请输入所用的文件名：");
+  // scanf("%s", filename);
+  // if ((fp = fopen(filename, "w")) == NULL)
+  // {
+  //   printf("无法打开此文件\n");
+  //   exit(0);
+  // }
+  // ch = getchar();
+  // printf("请输入一个准备存储到磁盘的字符串(以#结束：");
+  // ch = getchar();
+  // while (ch != '#')
+  // {
+  //   fputc(ch, fp);
+  //   putchar(ch);
+  //   ch = getchar();
+  // }
+  // fclose(fp);
+  // putchar(10);
+
+  // 文件的格式化读写
 }
