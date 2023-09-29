13e0e32b8447d9fd5673121081843537bde878a6
shuhongxie <38418258@qq.com>
2023-09-29 16:50:39 +0800

    feat: 增加

--- C语言设计/练习/第四章/demo.c
+++ C语言设计/练习/第四章/demo.c
@@ -2,38 +2,38 @@
 
 void main()
 {
-    // int a;
-    // if (true)
-    // {
-    //     a = 100;
-    // }
+  // int a;
+  // if (true)
+  // {
+  //     a = 100;
+  // }
 
-    // else
-    // {
-    //     if (false)
-    //     {
-    //         a = 300;
-    //     }
-    //     a = 200;
-    // }
-    // printf("a=%d", a);
+  // else
+  // {
+  //     if (false)
+  //     {
+  //         a = 300;
+  //     }
+  //     a = 200;
+  // }
+  // printf("a=%d", a);
 
-    // int a = 1;
-    // switch (a)
-    // {
-    // case 1:
-    //     printf("是第一");
-    // case 2:
-    //     printf("是第二");
-    // default:
-    //     printf("是默认");
-    // }
+  int a = 1;
+  switch (a)
+  {
+  case 1:
+    printf("是第一\n");
+  case 2:
+    printf("是第二\n");
+  default:
+    printf("是默认\n");
+  }
 
-    double x, fractpart, intpart;
+  // double x, fractpart, intpart;
 
-    x = 1234;
-    fractpart = modf(x, &intpart);
+  // x = 1234;
+  // fractpart = modf(x, &intpart);
 
-    printf("整数部分 = %lf\n", intpart);
-    printf("小数部分 = %lf \n", fractpart);
+  // printf("整数部分 = %lf\n", intpart);
+  // printf("小数部分 = %lf \n", fractpart);
 }
