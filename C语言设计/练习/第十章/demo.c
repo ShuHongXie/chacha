a0134b03c7e6f90ff42e166e74a7a9d3c669af5b
shuhongxie <38418258@qq.com>
2023-10-08 17:56:21 +0800

    feat: 增加

--- C语言设计/练习/第十章/demo.c
+++ C语言设计/练习/第十章/demo.c
@@ -12,26 +12,26 @@ void main()
   // }
 
   // 从键盘输入字符，然后送到磁盘上去
-  // FILE *fp;
-  // char ch, filename[10];
-  // printf("请输入所用的文件名：");
-  // scanf("%s", filename);
-  // if ((fp = fopen(filename, "w")) == NULL)
-  // {
-  //   printf("无法打开此文件\n");
-  //   exit(0);
-  // }
-  // ch = getchar();
-  // printf("请输入一个准备存储到磁盘的字符串(以#结束：");
-  // ch = getchar();
-  // while (ch != '#')
-  // {
-  //   fputc(ch, fp);
-  //   putchar(ch);
-  //   ch = getchar();
-  // }
-  // fclose(fp);
-  // putchar(10);
+  FILE *fp;
+  char ch, filename[10];
+  printf("请输入所用的文件名：");
+  scanf("%s", filename);
+  if ((fp = fopen("t5.txt", "r")) == NULL)
+  {
+    printf("无法打开此文件\n");
+    exit(0);
+  }
+  ch = getchar();
+  printf("请输入一个准备存储到磁盘的字符串(以#结束：");
+  ch = getchar();
+  while (ch != '#')
+  {
+    fputc(ch, fp);
+    putchar(ch);
+    ch = getchar();
+  }
+  fclose(fp);
+  putchar(10);
 
   // 文件的格式化读写
 }
