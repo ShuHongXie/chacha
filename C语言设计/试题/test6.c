eb14be6f0643e51c4c8c5e9bbd0f76d849610156
shuhongxie <38418258@qq.com>
2023-10-23 17:54:10 +0800

    feat: 增加

--- C语言设计/试题/test6.c
+++ C语言设计/试题/test6.c
@@ -4,6 +4,13 @@ getMax(int a, int b)
 {
   return a + b;
 }
+
+void testFunc(int *b, char *c)
+{
+  *b = 3;
+  *(c + 1) = 'j';
+}
+
 void main()
 {
   // int t, a, b, i;
@@ -25,7 +32,25 @@ void main()
   //   printf("%d", t);
   // }
 
-  int a = 3, b = 4, c;
-  c = getMax(a, b);
-  printf("%d", c);
+  // 验证函数默认值是否为int
+  // int a = 3, b = 4, c;
+  // c = getMax(a, b);
+  // printf("%d", c);
+
+  // 最大公约数
+  // int r;
+  // int m = 12, n = 9;
+  // while ((r = m % n) != 0)
+  // {
+  //   m = n;
+  //   n = r;
+  //   printf("%d,%d\n", m, n);
+  // }
+  // printf("%d", n);
+
+  // 函数指针使用整型传输时的情况
+  int a = 100;
+  char c[] = "asd";
+  testFunc(&a, c);
+  printf("%d,%s", a, c);
 }
