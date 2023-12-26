a3f4dea0c85ade230fdcbbf559663025e688779e
shuhongxie <38418258@qq.com>
2023-12-26 17:19:06 +0800

    feat: 增加

--- C语言数据结构/tempCodeRunnerFile.c
+++ C语言数据结构/tempCodeRunnerFile.c
@@ -1,11 +1,112 @@
-for (int i = 1; i < len; i++)
+#include <stdio.h>
+
+binarySearch()
+{
+}
+
+void main()
+{
+  int arr[] = {4, 2, 1, 5, 10, 6};
+  int len = 6;
+  int arr1[] = {1, 3, 7, 9, 11, 22, 63};
+
+  // 插入排序
+  // 选择排序
+  for (int i = 0; i < len; i++)
   {
-    int j = i - 1;
-    int curr = arr[i];
-    while (j >= 0 && arr[j] > curr)
+    int index = i;
+    for (int j = i + 1; j < len - 1; j++)
     {
-      arr[j + 1] = arr[j];
-      j--;
+      if (arr[j] < arr[index])
+      {
+        index = j;
+      }
     }
-    arr[j + 1] = curr;
+    if (index != i)
+    {
+      int temp = arr[i];
+      arr[i] = arr[index];
+      arr[index] = temp;
+    }
   }
+
+  printf("查询值为6的下标为%d", binarySearch(arr1, 7, 1));
+  // 遍历
+  for (int i = 0; i < 6; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// 二分查找
+// int binarySearch(int *a, int n, int key)
+// {
+
+//   int low, high, mid;
+//   low = 1;
+//   high = n;
+//   while (low <= high)
+//   {
+//     mid = (low + high) / 2;
+//     if (key < a[mid])
+//     {
+//       high = mid - 1;
+//     }
+//     else if (key > a[mid])
+//     {
+//       low = mid + 1;
+//     }
+//     else
+//     {
+//       return mid;
+//     }
+//   }
+// }
+
+// 鎻掑叆鎺掑簭
+// for (int i = 1; i < len; i++)
+// {
+//   int key = arr[i];
+//   int j = i - 1;
+//   while (j >= 0 && arr[j] > key)
+//   {
+//     arr[j + 1] = arr[j];
+//     j--;
+//   }
+//   arr[j + 1] = key;
+// }
+
+// 閫夋嫨鎺掑簭
+// for (int i = 0; i < len - 1; i++)
+// {
+//   int index = i;
+//   for (int j = i + 1; j < len; j++)
+//   {
+//     if (arr[j] < arr[index])
+//     {
+//       index = j;
+//     }
+//   }
+//   if (index != i)
+//   {
+//     int temp = arr[i];
+//     arr[i] = arr[index];
+//     arr[index] = temp;
+//   }
+// }
--- C语言数据结构/test.c
+++ C语言数据结构/test.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-// 二分查找
-int binarySearch(int *a, int n, int key)
+int binarySearch(int arr[], int len, int key)
 {
-
-  int low, high, mid;
-  low = 1;
-  high = n;
+  int low = 1;
+  int high = len;
+  int mid;
   while (low <= high)
   {
-    mid = (low + high) / 2;
-    if (key < a[mid])
-    {
-      high = mid - 1;
-    }
-    else if (key > a[mid])
+    mid = (high + low) / 2;
+    if (key > arr[mid])
     {
       low = mid + 1;
+    }
+    else if (key < arr[mid])
+    {
+      high = mid - 1;
     }
     else
     {
@@ -28,28 +26,10 @@ int binarySearch(int *a, int n, int key)
 void main()
 {
   int arr[] = {4, 2, 1, 5, 10, 6};
+  int len = 6;
   int arr1[] = {1, 3, 7, 9, 11, 22, 63};
 
-  // 閫夋嫨鎺掑簭
-  // for (int i = 0; i < len - 1; i++)
-  // {
-  //   int curr = i;
-  //   for (int j = i + 1; j < len; j++)
-  //   {
-  //     if (arr[j] < arr[curr])
-  //     {
-  //       curr = j;
-  //     }
-  //   }
-  //   if (curr != i)
-  //   {
-  //     int temp = arr[i];
-  //     arr[i] = arr[curr];
-  //     arr[curr] = temp;
-  //   }
-  // }
-
-  // 鎻掑叆鎺掑簭
+  // 插入排序
   // for (int i = 1; i < len; i++)
   // {
   //   int curr = arr[i];
@@ -62,8 +42,27 @@ void main()
   //   arr[j + 1] = curr;
   // }
 
-  printf("查询值为6的下标为%d", binarySearch(arr1, 7, 1));
-  // 鎵撳嵃
+  // 选择排序
+  // for (int i = 0; i < len - 1; i++)
+  // {
+  //   int index = i;
+  //   for (int j = i + 1; j < len; j++)
+  //   {
+  //     if (arr[j] < arr[index])
+  //     {
+  //       index = j;
+  //     }
+  //   }
+  //   if (index != i)
+  //   {
+  //     int temp = arr[i];
+  //     arr[i] = arr[index];
+  //     arr[index] = temp;
+  //   }
+  // }
+
+  printf("查询值为3的下标为%d", binarySearch(arr1, 7, 3));
+  // 遍历
   // for (int i = 0; i < 6; i++)
   // {
   //   printf("%d ", arr[i]);
@@ -85,6 +84,32 @@ void main()
 //
 //
 //
+
+// 二分查找
+// int binarySearch(int *a, int n, int key)
+// {
+
+//   int low, high, mid;
+//   low = 1;
+//   high = n;
+//   while (low <= high)
+//   {
+//     mid = (low + high) / 2;
+//     if (key < a[mid])
+//     {
+//       high = mid - 1;
+//     }
+//     else if (key > a[mid])
+//     {
+//       low = mid + 1;
+//     }
+//     else
+//     {
+//       return mid;
+//     }
+//   }
+// }
+
 // 鎻掑叆鎺掑簭
 // for (int i = 1; i < len; i++)
 // {
