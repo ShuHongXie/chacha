f3bee9b498ca51cc52bfa992c974d17ed82d00b0
shuhongxie <384180258@qq.com>
2024-02-02 00:27:05 +0800

    feat: 增加

--- C语言数据结构/test.c
+++ C语言数据结构/test.c
@@ -8,7 +8,7 @@ void binarySearch(int arr[], int len, int key)
   int mid;
   while (low <= high)
   {
-    mid = (low + high) / 2;
+    mid = (high + low) / 2;
     if (key > arr[mid])
     {
       low = mid + 1;
@@ -19,57 +19,87 @@ void binarySearch(int arr[], int len, int key)
     }
     else
     {
-      printf("关键字为%d的下标为%d\n", key, mid);
+      printf("查找关键字等于%d的值的下标为%d", key, mid);
       return;
     }
   }
-  printf("关键字为%d的下标未找到", key);
+  printf("关键字%d不在数组内", key);
+}
+
+// 插入排序
+void insertSort(int arr[], int len)
+{
+  for (int i = 1; i < len; i++)
+  {
+    int j = i - 1;
+    int curr = arr[i];
+    while (j >= 0 && arr[j] > curr)
+    {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = curr;
+  }
+}
+
+// 选择排序
+void selectSort(int arr[], int len)
+{
+  for (int i = 0; i < len - 1; i++)
+  {
+    int index = i;
+    for (int j = i + 1; j < len; j++)
+    {
+      if (arr[j] < arr[index])
+      {
+        index = j;
+      }
+    }
+    if (i != index)
+    {
+      int curr = arr[i];
+      arr[i] = arr[index];
+      arr[index] = curr;
+    }
+  }
+}
+
+// 冒泡排序
+void bubbleSort(int arr[], int len)
+{
+  for (int i = 0; i < len; i++)
+  {
+    for (int j = 0; j < len - i - 1; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
+        int num = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = num;
+      }
+    }
+  }
 }
 
 void main()
 {
+  void insertSort(int arr[], int len);
+  void selectSort(int arr[], int len);
+  void bubbleSort(int arr[], int len);
   int arr[] = {4, 2, 1, 5, 10, 6};
   int len = 6;
   int arr1[] = {1, 3, 7, 9, 11, 22, 63};
 
-  // 选择排序
-  // for (int i = 0; i < len - 1; i++)
-  // {
-  //   int index = i;
-  //   for (int j = i + 1; j < len; j++)
-  //   {
-  //     if (arr[index] > arr[j])
-  //     {
-  //       index = j;
-  //     }
-  //   }
-  //   if (index != i)
-  //   {
-  //     int temp = arr[i];
-  //     arr[i] = arr[index];
-  //     arr[index] = temp;
-  //   }
-  // }
+  // insertSort(arr, len);
+  // selectSort(arr, len);
+  bubbleSort(arr, len);
 
-  // 插入排序
-  // for (int i = 1; i < len; i++)
-  // {
-  //   int j = i - 1;
-  //   int curr = arr[i]; // 这里一定要保留，不然会在下面的arr[j+1] = arr[j]被改变掉
-  //   while (j >= 0 && arr[j] > curr)
-  //   {
-  //     arr[j + 1] = arr[j];
-  //     j--;
-  //   }
-  //   arr[j + 1] = curr;
-  // }
-
-  binarySearch(arr1, 7, 3);
+  // binarySearch(arr1, 7, 100);
   // 遍历
-  // for (int i = 0; i < len; i++)
-  // {
-  //   printf("%d ", arr[i]);
-  // }
+  for (int i = 0; i < len; i++)
+  {
+    printf("%d ", arr[i]);
+  }
 }
 //
 //
